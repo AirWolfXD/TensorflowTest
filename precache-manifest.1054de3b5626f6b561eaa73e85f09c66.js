417cd394b1d48ce795e70160f08010f2ac66a826
Andrew Yu <2521andy@gmail.com>
2021-04-20 16:22:54 -0700

    Updates

---
+++ precache-manifest.1054de3b5626f6b561eaa73e85f09c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d32052d2a3bc4b493eeacfb0026d6a",
+    "url": "/TensorflowTest/index.html"
+  },
+  {
+    "revision": "87561ec73d87524ac35d",
+    "url": "/TensorflowTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "664b1394f1cc06efd819",
+    "url": "/TensorflowTest/static/js/2.4282fdb3.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/TensorflowTest/static/js/2.4282fdb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87561ec73d87524ac35d",
+    "url": "/TensorflowTest/static/js/main.8214f0cf.chunk.js"
+  },
+  {
+    "revision": "1f173f15358b2bec12e9",
+    "url": "/TensorflowTest/static/js/runtime-main.4b26d04c.js"
+  }
+]);
